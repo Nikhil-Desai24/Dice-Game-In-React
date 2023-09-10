8f116c6ce2bafb8b80f266bc39810ea1d89b0170
Nikhil-Desai24 <nikhildesaind024@gmail.com>
2023-09-10 17:20:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import StartGame from "./components/StartGame"
+import GamePlay from "./components/GamePlay";
+// import TotalScore from "./components/TotalScore";
+function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+    console.log(isGameStarted);
+  }
+
+
+  return (
+   <>
+   {
+   isGameStarted ? <GamePlay/> : <StartGame toggle = {toggleGamePlay}/>
+   }
+   </>
+  )
+}
+
+export default App;
